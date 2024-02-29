94bf6bffa1bc53b50f70c023656c5368447f30e6
pavankumar4404 <pavan4404kpk@gmail.com>
2024-02-29 21:01:13 +0530

    cf div2

---
+++ A_Shuffle_Party.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+// Macros
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+
+#define yes cout<<"YES"<<'\n'
+#define no cout<<"NO"<<'\n'
+#define inp(x) int x; cin>>x;
+#define br cout<<'\n'
+
+// Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+
+void solve(){
+    ll n;
+    cin>>n;
+    ll p = log2(n);
+    cout<<(1<<p);
+    br;
+    
+}
+
+int32_t main()
+{
+    fastio()
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
