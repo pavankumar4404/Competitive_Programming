426f6dd24464d75dd39a6328400ad2633fab5bf9
pavankumar4404 <pavan4404kpk@gmail.com>
2024-03-14 23:39:29 +0530

    practice

---
+++ A_Don_t_Try_to_Count.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+// Macros
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+
+#define yes cout<<"YES"<<'\n'
+#define no cout<<"NO"<<'\n'
+#define inp(x) int x; cin>>x;
+#define br cout<<'\n'
+
+// Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+
+void solve(){
+    inp(n);
+    inp(m);
+    string a;
+    string b;
+    cin>>a;
+    cin>>b;
+
+    for(int i=0;i<=5;i++){
+        if(a.find(b)!=string::npos){
+            cout<<i;br;
+            return;
+        }
+        a+=a;
+    }
+    cout<<-1;br;
+}
+
+int32_t main()
+{
+    fastio()
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ A_How_Much_Does_Daytona_Cost.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+// Macros
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+
+#define yes cout<<"YES"<<'\n'
+#define no cout<<"NO"<<'\n'
+#define inp(x) int x; cin>>x;
+#define br cout<<'\n'
+
+// Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+
+void solve(){
+    inp(n);
+    inp(k);
+    unordered_map<int,int> mp;
+    // int maxi=0;
+    for(int i=0;i<n;i++){
+        inp(x);
+        mp[x]++;
+        // if(mp[x]>maxi) maxi=mp[x];
+    }
+    if(mp[k]>=1){
+        yes;
+    }
+    else no;
+}
+
+int32_t main()
+{
+    fastio()
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
