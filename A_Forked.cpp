af0cf98b5c09b819e514095804e47ce000d65b56
pavankumar4404 <pavan4404kpk@gmail.com>
2024-03-28 19:36:31 +0530

    cp 900

---
+++ A_Forked.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+// Macros
+typedef long long ll;
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+#define pb insert
+
+// Binary
+// string decToBinary(int n){string s="";int i = 0;while (n > 0) {s =to_string(n % 2)+s;n = n / 2;i++;}return s;},
+// ll binaryToDecimal(string n){string num = n;ll dec_value = 0;int base = 1;int len = num.length();for(int i = len - 1; i >= 0; i--){if (num[i] == '1')dec_value += base;base = base * 2;}return dec_value;}
+
+//shortcuts
+#define yes cout<<"YES";
+#define no cout<<"NO";
+#define inp(x) int x; cin>>x;
+#define inpl(x) ll x; cin>>x;
+#define inp2(x,y) int x,y; cin>>x>>y;
+#define inp2l(x,y) ll x,y; cin>>x>>y;
+#define inp3(x,y,z) int x,y,z; cin>>x>>y>>z;
+#define inp3l(x,y,z) ll x,y,z; cin>>x>>y>>z;
+#define br cout<<'\n';
+#define out(x) cout<<x;
+#define outln(x) cout<<x<<'\n';
+
+// Mathematical functions
+#define ll long long
+ll factorial(ll n) {if(n==0) return 1; ll res = 1; for (ll i=2; i<=n; i++) res=res*i; return res;}
+ll nPr(ll n, ll r) { return factorial(n) / factorial(n - r); }
+ll nCr(ll n, ll r) { return factorial(n) / (factorial(r) * factorial(n - r));}
+ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a * b) / gcd(a, b);}
+bool isPrime(ll n) {if(n<=1)return false;if(n<=3)return true;if(n%2==0||n%3==0)return false;for(int i=5;i*i<=n;i=i+6)if(n%i==0||n%(i+2)==0)return false;return true;}
+
+// Typedef
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+typedef set<int> si;
+typedef unordered_map<int,int> umii;
+typedef unordered_map<char,int> umci;
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+
+set<pair<ll,ll>> generate(ll a, ll b, ll x, ll y){
+    set<pair<ll,ll>> arr;
+    arr.pb({x+a,y+b});
+    arr.pb({x-a,y+b});
+    arr.pb({x+a,y-b});
+    arr.pb({x-a,y-b});
+    arr.pb({x+b,y+a});
+    arr.pb({x+b,y-a});
+    arr.pb({x-b,y+a});    
+    arr.pb({x-b,y-a});
+    return arr;
+}
+
+void solve(){
+    inp2l(a,b);
+    inp2l(kx,ky);
+    inp2l(qx,qy);
+    set<pair<ll,ll>> arrk=generate(a,b,kx,ky);
+    set<pair<ll,ll>> arrq=generate(a,b,qx,qy);
+    int ans=0;
+    // for(int i=0;i<7;i++){
+    //     for(int j=i+1;j<8;j++){
+    //         if(arrk[i].first==arrq[j].first && arrk[i].second==arrq[j].second){
+    //             ans++;
+    //         }
+    //     }
+    // }
+    for(auto x:arrk){
+        if(arrq.find(x)!=arrq.end()){
+            ans++;
+        }
+    }
+    out(ans);
+
+}
+
+int32_t main()
+{
+    fastio()
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+        br;
+    }
+    return 0;
+}
